f4306b27f87f6b67190a2dba5d883b3f8c5ef352
Bhavishyaa Vignesh <67924047+bhavicodey@users.noreply.github.com>
2021-07-22 17:14:04 -0700

    Add files via upload

--- intersender_room.js
+++ intersender_room.js
@@ -1,10 +1,47 @@
 
-//ADD YOUR FIREBASE LINKS HERE
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBzQ4SM943Oq0XXibO_E3XRugOyS0tL8io",
+    authDomain: "intersender.firebaseapp.com",
+    projectId: "intersender",
+    storageBucket: "intersender.appspot.com",
+    messagingSenderId: "769303829687",
+    appId: "1:769303829687:web:5ee6b3fb12baadc9960132"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  user_name = localStorage.getItem("user_name");
+  document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+  function addRoom() {
+    room_name = document.getElementById("room_name").value;
+    firebase.database().ref ("/").child(room_name).update({
+          purpose: "adding room name"
+    });
+    localStorage.setItem("room_name");
+    window.location = "intersender_page.html";      
+  }
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-
-      //End code
-      });});}
+     Room_names = childKey;
+    //Start code 
+    console.log("Room Name - " + Room_names);
+    row = "<div class='room_name' id="+ Room_names + " onclick = 'redirectToRoomName(this.id)' > #" + Room_names + "</div> <hr>";
+    document.getElementById("output").innerHTML += row; 
+    //End code
+    });});}
 getData();
+
+function redirectToRoomName(name) {
+    console.log(name);
+    localStorage.setItem("room_name");
+    window.location = "intersender_page.html";
+}
+
+function logout() {
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("remove_name");
+    window.location = "index.html";
+}
+
